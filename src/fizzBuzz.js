a94c521b0c6a9e844d127202c76e3aa1013a4284
jason00111 <emberleyjason@gmail.com>
2017-05-23 21:40:21 -0700

    completes fizzBuzz

---
+++ src/fizzBuzz.js
@@ -0,0 +1,12 @@
+export default function fizzBuzz() {
+  const result = []
+
+  for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) result[i - 1] = 'FizzBuzz'
+    else if (i % 3 === 0) result[i - 1] = 'Fizz'
+    else if (i % 5 === 0) result[i - 1] = 'Buzz'
+    else result[i - 1] = i
+  }
+
+  return result
+}
---
+++ test/fizzBuzz_test.js
@@ -0,0 +1,61 @@
+import { expect } from 'chai'
+import fizzBuzz from '../src/fizzBuzz'
+
+describe('fizzBuzz()', function(){
+
+  it('should be a function', function(){
+    expect(fizzBuzz).to.be.a('function')
+  })
+
+  it('returns an array of length 100', function(){
+    const result = fizzBuzz()
+    expect(result).to.be.an('array')
+    expect(result).to.have.length(100)
+  })
+
+  it('entries at positions which are multiples of 3 and 5 are FizzBuzz', function(){
+    const multiplesOf3and5 = fizzBuzz()
+      .filter( (element, index) =>
+        (index + 1) % 3 === 0 && (index + 1) % 5 === 0
+      )
+
+    expect(multiplesOf3and5.every(
+      element => element === 'FizzBuzz'
+    )).to.be.true
+  })
+
+  it('entries at positions which are multiples of 3 and not 5 are Fizz', function(){
+    const multiplesOf3not5 = fizzBuzz()
+      .filter( (element, index) =>
+        (index + 1) % 3 === 0 && (index + 1) % 5 !== 0
+      )
+
+    expect(multiplesOf3not5.every(
+      element => element === 'Fizz'
+    )).to.be.true
+  })
+
+  it('entries at positions which are multiples of 5 and not 3 are Buzz', function(){
+    const multiplesOf5not3 = fizzBuzz()
+      .filter( (element, index) =>
+        (index + 1) % 3 !== 0 && (index + 1) % 5 === 0
+      )
+
+    expect(multiplesOf5not3.every(
+      element => element === 'Buzz'
+    )).to.be.true
+  })
+
+  it('entries at positions which are not multiples of 5 or 3 are numbers corresponding to their position', function(){
+    const notMultiplesOf5or3 = fizzBuzz()
+      .map( (element, index) => ({element, index}) )
+      .filter( ({element, index}) =>
+        (index + 1) % 3 !== 0 && (index + 1) % 5 !== 0
+      )
+
+    expect(notMultiplesOf5or3.every(
+      ({element, index}) => element === index + 1
+    )).to.be.true
+  })
+
+})
